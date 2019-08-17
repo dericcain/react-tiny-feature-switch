ca4c0f6829d040a90f9a6e063f09965c006d976f
Deric Cain <deric.cain@gmail.com>
2019-08-17 06:07:44 -0500

    Finished testing

--- test/feature-toggler.test.tsx
+++ test/feature-toggler.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { render as rtlRender } from '@testing-library/react';
+import { render as rtlRender, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
-import { FeatureToggle, FeatureToggler } from '../src';
+import { FeatureToggle, FeatureToggler, FeatureSwitch } from '../src';
 import { FeatureTogglerProps } from '../src/feature-toggler';
 
 const features = {
@@ -39,5 +39,35 @@ describe('<FeatureToggler />', () => {
     expect(getByTestId('feature-toggler-checkbox-firstName')).toHaveAttribute('checked');
     expect(getByTestId('feature-toggler-checkbox-lastName')).toHaveAttribute('checked');
     expect(getByTestId('feature-toggler-checkbox-address')).not.toHaveAttribute('checked');
+  });
+
+  it('should toggle features on and off', () => {
+    const App = () => (
+      <Wrapper>
+        <FeatureSwitch features="firstName">
+          <span>Jon</span>
+        </FeatureSwitch>
+        <FeatureSwitch features="lastName">
+          <span>Appleseed</span>
+        </FeatureSwitch>
+        <FeatureSwitch features="address">
+          <span>123 Main Street</span>
+        </FeatureSwitch>
+        <FeatureToggler alwaysShow={true} />
+      </Wrapper>
+    );
+    const { queryByText, getByTestId } = rtlRender(<App />);
+
+    expect(queryByText(/jon/i)).toBeInTheDocument();
+    expect(queryByText(/appleseed/i)).toBeInTheDocument();
+    expect(queryByText(/123 main/i)).not.toBeInTheDocument();
+
+    fireEvent.click(getByTestId('feature-toggler-checkbox-firstName'));
+    fireEvent.click(getByTestId('feature-toggler-checkbox-lastName'));
+    fireEvent.click(getByTestId('feature-toggler-checkbox-address'));
+
+    expect(queryByText(/jon/i)).not.toBeInTheDocument();
+    expect(queryByText(/appleseed/i)).not.toBeInTheDocument();
+    expect(queryByText(/123 main/i)).toBeInTheDocument();
   });
 });
